2129aa94b71607437d1f67772676e6cd4e264ebd
Itay Mishan <mishan.itay@gmail.com>
2016-04-26 13:11:15 +0300

    Update ruby_es_query.rb

--- ruby_es_query.rb
+++ ruby_es_query.rb
@@ -12,7 +12,7 @@ class FetchPackQuery < ActiveQuery
                   query: { match_all: {} },
                   filter: {
                     and: [
-                      {term: {'xx.xx': 4 }},
+                      {term: {'x.xx': 4 }},
                       {term: {'xx.xx': 2 }}
                     ]
                   }
